471b65f334aeafb4fabbacdddb72668302eb8977
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-08-12 11:39:28 +0530

    Add pseudocode for Bubble Sort

---
+++ bubble-sort.rb
@@ -0,0 +1,13 @@
+def bubble_sort(array)
+  # PSEUDOCODE
+  # SET length to array.length
+  # REPEAT
+  #   SET swapped to false
+  #   FOR each time in 1..(length - 1)
+  #     IF prev element is greater than curr element THEN
+  #       SWAP the prev element with the curr element
+  #       SET swapped to true
+  #     ENDIF
+  #   ENDFOR
+  # UNTIL swapped is false
+end
